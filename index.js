340bfb2240c269f727cd0fd761b25a292e57f852
hp <inomjonovadilyorabonu@gmail.com>
2024-03-29 02:08:21 +0500

    Finished

---
+++ index.js
@@ -0,0 +1,28 @@
+let a = prompt('1 dan 7 gacha son kiriting')
+if (1 > a){
+    console.log(a + '= bu son 1dan kichik')
+}
+if (8 <= a){
+    console.log(a + '= bu son 7dan katta')
+}
+if (a == 1){
+    console.log('2 = sewanba')
+}
+if (a == 2){
+    console.log('2 = sewanba')
+}
+if (a == 3){
+    console.log('3 = chorwanba')
+}
+if (a == 4){
+    console.log('4 = paywanba')
+}
+if (a == 5){
+    console.log('5 = juma')
+}
+if (a == 6){
+    console.log('6 = wanba')
+}
+if (a == 7){
+    console.log('7 = yakwanba')
+}
